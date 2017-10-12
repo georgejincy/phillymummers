ee806053691d61b492dda23b625aa1ebf353ebb9
Jincy George <jincy.george@bhemail.com>
2017-10-12 11:46:41 -0400

    :tractor: Switching to new blockchain tutorial
    https://jeiwan.cc/posts/building-blockchain-in-go-part-1/

---
+++ blockchain.go
@@ -0,0 +1,19 @@
+package main 
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
